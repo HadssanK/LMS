613dbfe34fef4ff173d56a3552f8efb43267eb37
AL HABIB TRADE <hassankhan847754@gmail.com>
2025-05-29 14:28:11 -0700

    Fix: Remove embedded git from frontend folder

--- frontend/src/Components/generateContante.jsx
+++ frontend/src/Components/generateContante.jsx
@@ -18,7 +18,7 @@ const handleGenerate = async () => {
 
   try {
     const response = await axios.post(
-      `$http://localhost:5000/api/generate`,
+      `http://localhost:5000/api/generate`,
       { topic }
     );
     setContent(response.data.generated_text);
